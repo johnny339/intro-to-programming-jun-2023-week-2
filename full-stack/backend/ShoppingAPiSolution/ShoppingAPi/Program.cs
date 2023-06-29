89d97b4a2839de257879ecce024f8676dd9d5515
Johnny Guzman <guzmanj774@gmail.com>
2023-06-29 14:14:18 -0400

    fixing API issue

--- full-stack/backend/ShoppingAPiSolution/ShoppingAPi/Program.cs
+++ full-stack/backend/ShoppingAPiSolution/ShoppingAPi/Program.cs
@@ -23,6 +23,16 @@ builder.Services.AddDbContext<ShoppingDataContext>(options =>
 builder.Services.AddScoped<ILookupTheStatus, StatusLookup>();
 builder.Services.AddScoped<IManageTheShoppingList, PostgresShoppingManager>();
 
+builder.Services.AddCors(builder =>
+{
+    builder.AddDefaultPolicy(pol =>
+    {
+        pol.AllowAnyOrigin();
+        pol.AllowAnyHeader();
+        pol.AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,7 +42,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors();
 
 app.UseAuthorization();
 
